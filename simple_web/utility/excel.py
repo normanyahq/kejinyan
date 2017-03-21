73c9f487522431da659c834ddf30542726d1ca35
Heqing Ya <normanyahq@gmail.com>
2017-03-21 00:10:28 -0700

    support filter students to get related scorse

--- simple_web/utility/excel.py
+++ simple_web/utility/excel.py
@@ -141,7 +141,7 @@ def generateXlsx(output, standard_answers, student_info, credits=None):
     format_percentage.set_num_format(0xA)
 
     stats_sheet.write_string(0, 0, u"总人数", bold)
-    stats_sheet.write_formula(0, 1, u'=COUNTA(学生成绩!A:A)-1', center)
+    stats_sheet.write_formula(0, 1, u'=SUBTOTAL(103, 学生成绩!A:A)-1', center)
 
     stats_sheet.write_string(1, 0, u"题号", bold)
     stats_sheet.write_string(1, 1, u"答案", bold)
@@ -152,21 +152,69 @@ def generateXlsx(output, standard_answers, student_info, credits=None):
     for i in range(num_question):
         stats_sheet.write_number(i+2, 0, i+1, bold)
         stats_sheet.write_formula(i+2, 1, u'=答案与分值!B{}'.format(i+2), bold)
+
+        # Extremely complicated, it took me 1 hour:
+        # https://exceljet.net/formula/count-visible-rows-only-with-criteria
+        visible_correct = u'=SUMPRODUCT((--(学生成绩!{}2:{}{}=ans_trans!{}2))' + \
+            u'*(SUBTOTAL(103,OFFSET(学生成绩!{}2,ROW(学生成绩!{}2:{}{})' + \
+            u'-MIN(ROW(学生成绩!{}2:{}{})),0))))'
+
+        # TODO: optimize this part
         stats_sheet.write_formula(i+2,
                                   2,
-                                  u"=COUNTIF(学生成绩!{}2:{}{}, ans_trans!{}2)".format(getColName(i+2),
-                                                                                   getColName(i+2),
-                                                                                   num_student+1,
-                                                                                   getColName(i+1)),
+                                  visible_correct.format(getColName(i+2),
+                                                   getColName(i+2),
+                                                   num_student+1,
+                                                   getColName(i+1),
+                                                   getColName(i+2),
+                                                   getColName(i+2),
+                                                   getColName(i+2),
+                                                   num_student+1,
+                                                   getColName(i+2),
+                                                   getColName(i+2),
+                                                   num_student+1,),
                                   center)
+
+        # stats_sheet.write_formula(i+2,
+        #                           2,
+        #                           u"=COUNTIF(学生成绩!{}2:{}{}, ans_trans!{}2)".format(getColName(i+2),
+        #                                                                            getColName(i+2),
+        #                                                                            num_student+1,
+        #                                                                            getColName(i+1)),
+        #                           center)
+
+        # the number of visible students who chose choice_k
+        # shit, it's even more absurdfuckingly complicated
+        # I don't expect I can understand it in the future
+        visible_choose = u'=SUMPRODUCT(((学生成绩!{}2:{}{}="{}"))' + \
+            u'*(SUBTOTAL(103,OFFSET(学生成绩!{}2,ROW(学生成绩!{}2:{}{})' + \
+            u'-MIN(ROW(学生成绩!{}2:{}{})),0))))/B1'
+
+        # TODO: optimize this part
         for j, v in enumerate('ABCDE'):
             stats_sheet.write_formula(i+2,
                                       j+4,
-                                      u'=COUNTIF(学生成绩!{}2:{}{}, "{}")/B1'.format(getColName(i+2),
-                                                                                       getColName(i+2),
-                                                                                       num_student+1,
-                                                                                       v),
+                                      visible_choose.format(getColName(i+2),
+                                                            getColName(i+2),
+                                                            num_student+1,
+                                                            v,
+                                                            getColName(i+2),
+                                                            getColName(i+2),
+                                                            getColName(i+2),
+                                                            num_student+1,
+                                                            getColName(i+2),
+                                                            getColName(i+2),
+                                                            num_student+1),
                                       format_percentage)
+
+        # for j, v in enumerate('ABCDE'):
+        #     stats_sheet.write_formula(i+2,
+        #                               j+4,
+        #                               u'=COUNTIF(学生成绩!{}2:{}{}, "{}")/B1'.format(getColName(i+2),
+        #                                                                                getColName(i+2),
+        #                                                                                num_student+1,
+        #                                                                                v),
+        #                               format_percentage)
 
 
         # in excel, the row number starts from 1, while
@@ -180,4 +228,6 @@ def generateXlsx(output, standard_answers, student_info, credits=None):
                                     'minimum': 0})
     note_sheet.merge_range("A1:Z1", u"1、可在“答案与分值”一表中修改每题对应分值，学生成绩会自动更新。")
     note_sheet.merge_range("A2:Z2", u"2、可在“学生成绩”一表修改学生成绩，相关数据会自动更新。如果有识别错误（虽然不太可能发生），请把相应的答题卡文件发送到psdn@qq.com以便我分析改进。")
+    note_sheet.merge_range("A3:Z3", u"3、本表格文档经过精心设计。可通过数据选项卡中的筛选筛选特定学生（如根据学号筛选特定班级的学生），获得被筛选出学生的统计数据。")
+
     workbook.close()
