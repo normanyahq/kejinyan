9ed9324c66d44205b38857d61159222eec24901e
Heqing Ya <normanyahq@gmail.com>
2017-07-02 23:55:36 -0700

    add settings.py

---
+++ simple_web/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+modeDictionary = {
+    u"exact": u"普通模式",
+    u"partial": u"高考模式",
+    u"half": u"半页答题卡（35题）",
+    u"full": u"全页答题卡（75题）",
+    u"gk_english": u"全国卷高考英语专用答题卡",
+    u"makesi": u"河池学院马克思主义学院考试专用答题卡",
+    u"full_4option": u"河池学院大学英语考试专用答题卡",
+}
